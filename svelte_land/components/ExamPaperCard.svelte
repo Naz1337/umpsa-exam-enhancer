8213fd096a5520cd7383f28a983b303cc3e328ac
Naz1337 <69098093+Naz1337@users.noreply.github.com>
2025-02-07 00:17:51 +0800

    I think we have something...

--- svelte_land/components/ExamPaperCard.svelte
+++ svelte_land/components/ExamPaperCard.svelte
@@ -1,20 +1,40 @@
 <script>
     let { paper } = $props();
 
-    paper = JSON.stringify(paper, null, 2);
+    // paper = JSON.stringify(paper, null, 2);
 </script>
 
 <style>
-    div {
+    .card {
         width: 100%;
         padding: 1rem;
         border: 1px solid #ccc;
         border-radius: 0.5rem;
         display: flex;
-        gap: 1rem;
-    }  
+
+        flex-direction: column;
+    }
+
+    .filename {
+        font-size: 1.25rem;
+        font-weight: 600;
+        color: #30030e;
+    }
+
+    .semester {
+        color: #670e24;
+
+        margin-bottom: 1rem;
+    }
+
+    .faculty {
+        color: #f5a3b9;
+        font-weight: 700;
+    }
 </style>
 
-<div>
-    {paper}
+<div class="card">
+    <div class="filename">{paper.exam_paper}</div>
+    <div class="semester">{paper.semester}</div>
+    <div class="faculty">{paper.faculty}</div>
 </div>
