7707f78026b95929a4128bb656d9bd3f1c4687ab
Naz1337 <69098093+Naz1337@users.noreply.github.com>
2025-02-06 21:52:54 +0800

    we are at early design phase rn

--- design_place/App.svelte
+++ design_place/App.svelte
@@ -1,5 +1,14 @@
 <script>
+    import ExamPaperCard from "../svelte_land/components/ExamPaperCard.svelte";
 
+    let paper = {
+        exam_paper: "BCI2313 ALGORITHM & COMPLEXITY S2 2024.pdf",
+        faculty: "FAKULTI KOMPUTERAN",
+        no: "2",
+        pdf_link: "https://apps-oss-ump-edu-my.libraryumpsa.idm.oclc.org/exam/uploads/BCI2313%20ALGORITHM%20&%20COMPLEXITY%20S2%202024.pdf",
+        semester: "SEMESTER II SESSION 2023/2024",
+        semester_numbered: 2023242
+    }
 </script>
 
 <style>
@@ -9,9 +18,22 @@
         flex-direction: column;
         gap: 1rem;
     }
+
+    div {
+        flex: 0 0 auto;
+        width: calc(600px + 2rem);
+        padding: 1.5rem;
+
+        border: 2px solid #000;
+
+        align-self: center;
+    }
 </style>
 
 <main>
     <h1>Design Place</h1>
-    
+
+    <div>
+        <ExamPaperCard {paper}/>
+    </div>
 </main>
---
+++ svelte_land/components/ExamPaperCard.svelte
@@ -0,0 +1,20 @@
+<script>
+    let { paper } = $props();
+
+    paper = JSON.stringify(paper, null, 2);
+</script>
+
+<style>
+    div {
+        width: 100%;
+        padding: 1rem;
+        border: 1px solid #ccc;
+        border-radius: 0.5rem;
+        display: flex;
+        gap: 1rem;
+    }  
+</style>
+
+<div>
+    {paper}
+</div>
