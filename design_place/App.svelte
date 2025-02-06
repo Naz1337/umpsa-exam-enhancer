25f0f6c535c16595beec8ea1159b86a3a3686529
Naz1337 <69098093+Naz1337@users.noreply.github.com>
2025-02-06 21:37:05 +0800

    we utilising hmr to prototype ui stuff now...

---
+++ design_place/App.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<style>
+    main {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
+</style>
+
+<main>
+    <h1>Design Place</h1>
+    
+</main>
