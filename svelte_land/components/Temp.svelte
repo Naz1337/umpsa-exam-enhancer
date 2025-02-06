ae8c5338f63625cdddd92daf703d10ab4ca023f5
Naz1337 <69098093+Naz1337@users.noreply.github.com>
2025-02-06 15:35:21 +0800

    now it knows how many paper there are on the site!

--- svelte_land/components/Temp.svelte
+++ svelte_land/components/Temp.svelte
@@ -1,15 +1,21 @@
 <script>
-    let message = "Hello, Svelte!";
+    let message = "Hello, this is Svelte!";
+
+    let { total_row } = $props();
 
     alert(message);
 </script>
 
 <style>
 h1 {
-    color: #42b983;
+    color: hsl(153, 47%, 49%);
+}
+
+span {
+    color: hsl(153, 67%, 19%);
 }
 </style>
 
 <main>
-    <h1>{message}</h1>
+    <h1>{message} There are <span>{ total_row }</span> papers to look at...</h1>
 </main>
--- svelte_land/main.js
+++ svelte_land/main.js
@@ -1,8 +1,21 @@
 import { mount } from 'svelte';
 import Temp from './components/Temp.svelte';
 
-const app = mount(Temp, {
-    target: document.body,
-})
 
-export default app;
+const exam_paper_table_selector = 'body > table:nth-child(5)';
+const exam_paper_table = document.querySelector(exam_paper_table_selector);
+
+if (exam_paper_table === null) {
+    throw new Error(`Could not find exam paper table with selector: ${exam_paper_table_selector}`);
+};
+
+const table_body = exam_paper_table.querySelector('tbody');
+const total_row = table_body.rows.length - 1;  // because the header for the table is not
+                                               // in thead but in tbody
+
+mount(Temp, {
+    target: document.body,
+    props: {
+        total_row,
+    }
+});
