fbcd2858a4737bff576d6419c1e53d1e8945f456
Naz1337 <69098093+Naz1337@users.noreply.github.com>
2025-02-15 12:52:11 +0800

    no minification in userscript cuz people want to read

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 export default defineConfig({
   plugins: [svelte()],
   build: {
+    minify: false,
     rollupOptions: {
       input: './svelte_land/main.js', // Entry point for your app
       output: {
